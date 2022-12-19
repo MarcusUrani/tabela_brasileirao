1d3d52bf9c54d5dfc89aa616b2aca893b4d7c8f6
Marcus <marcus.urani20@gmail.com>
2022-12-19 19:44:03 -0300

    Adicionando o registro de times

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct team
+{
+    int position;
+    char name[30];
+    int points;
+    int matches;
+    int victories;
+    int goal_difference;
+    int goals_for;
+    int goals_against;
+};
+
 
 int main(int argc, char **argv){
     return 0;
